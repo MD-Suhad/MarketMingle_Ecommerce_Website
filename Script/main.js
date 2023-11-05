fa3f7a22fac19585b7ca0f9b85a94c8993108027
MD-Suhad <ssuhad490@gmail.com>
2023-11-05 16:08:39 +0600

    project updated

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,34 @@
-console.log(
-"hii this is the javascript side"
-)
+const imperative = [
+   "React is declarative",
+   "It is fast",
+   "jsx is simple",
+   "It is component way",
+   "It is easy to learn",
+   "It is fun to use"
+];
+
+function whylearnReact(){
+      const whyReact = document.getElementById("why-react");
+      const heading = document.createElement("h2");
+      heading.innerText = "why-react?";
+      heading.classList.add("heading");
+      whyReact.appendChild(heading);
+        
+
+      for(let i = 0; i<imperative.length; i++){
+         const list = document.createElement("li");
+         list.innerText = imperative[i];
+         whyReact.appendChild(list);
+     }
+
+   }
+
+
+
+
+
+
+whylearnReact()
+
+
+
